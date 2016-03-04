6cb4910f435eab21aacd44c29e727cfbe5815317
Keaton Okkonen <keaton.okkonen@gmail.com>
2016-03-04 09:36:30 -1200

    frontend rework

--- Client/helpers/index.js
+++ Client/helpers/index.js
@@ -30,19 +30,39 @@ if (Meteor.isClient) {
                 var dataUrl = event.target.result;
                 template.dataUrl.set(dataUrl);
             }
+            $('#filename').text(file.name);
             fileReader.readAsDataURL(file);
         },
         // On form submit
-        "submit" : function(event,template) {
+        "submit" : function(event, template) {
             event.preventDefault();
             //the uploaded image in base64(meteor normally handles it automatically but see how it goes)
             var dataObject = template.dataUrl.get()
+            console.log(dataObject);
             Meteor.call("categorizeImages", dataObject, function(error, result) {
                 if(error){ console.log("error", error); }
                 if(result){
                     console.log(result);
                     //display results
                 }
+            });
+        },
+        // Button events
+        "click #upload-btn" : function(event,template) {
+            console.log(this);
+            // Open the 'upload' section and enable new button
+            $('#main').removeClass('active');
+            $('#upload').delay(800).queue(function (next) {
+                $(this).addClass('active');
+                next();
+            });
+        },
+        "click #about-btn" : function (event,template) {
+            // Close the 'about' section and disabled button
+            $('#upload').removeClass('active');
+            $('#main').delay(800).queue(function (next) {
+                $(this).addClass('active');
+                next();
             });
         }
     });
