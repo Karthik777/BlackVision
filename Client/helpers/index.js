d545e8e3ebe974f05fe99d3f02d0dc491ee02d78
LovelyHorse <keaton.okkonen@gmail.com>
2016-03-05 14:45:40 +1100

    backend api changes

--- Client/helpers/index.js
+++ Client/helpers/index.js
@@ -4,4 +4,18 @@ if (Meteor.isClient) {
             return true;
         }
     });
+    Template.index.events({
+      "click #event": function(event, template){
+        event.preventDefault();
+        var dataObject = //the uploaded image in base64(meteor normally handles it automatically but see how it goes)
+        Meteor.call("categorizeImages", dataObject, function(error, result){
+          if(error){
+            console.log("error", error);
+          }
+          if(result){
+             //display results
+          }
+        });
+      }
+    });
 }
--- helper.js
+++ helper.js
@@ -1,7 +1,5 @@
 // helper.js
-Animals = new Meteor.Collection("Animals");
-SpeciesSuite = new Meteor.Collection('AnimalList');
-Data = new Meteor.Collection('speciesData')
+Data = new Meteor.Collection('imageData')
 var createThumb = function(fileObj, readStream, writeStream) {
   // Transform the image into a 10x10px thumbnail
   gm(readStream, fileObj.name()).resize('200', '200').stream().pipe(writeStream);
